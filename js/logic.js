d5ea47240ccf695454aab39cdb60b43c47d58334
Akash <na.akash715@gmail.com>
2022-09-03 03:06:10 +0600

    sort by view done

--- js/logic.js
+++ js/logic.js
@@ -15,13 +15,34 @@ const displayCategories = (categories) => {
     // console.log(categories);
     const categoriesContainer = document.getElementById('categories-container');
     categories.forEach(category => {
-        console.log(category);
+        // console.log(category);
         const categoryDiv = document.createElement('div');
         categoryDiv.innerHTML = `
-        <button class="btn btn-outline-danger border-0 mx-2 text-nowrap fw-semibold" >${category.category_name}</button>
+        <button onclick="loadCategoryNews('${category.category_id}','${category.category_name}')" class=" btn btn-outline-danger border-0 mx-2 text-nowrap fw-semibold" >${category.category_name}</button>
         `;
         categoriesContainer.appendChild(categoryDiv);
     });
+
+}
+
+const loadCategoryNews = async (categoryId, categoryName) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        displayCategoryNews(data.data, categoryName);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+const displayCategoryNews = (data, categoryName) => {
+    console.log(data);
+    // setting number of items/news per category 
+    const itemsPerCategory = document.getElementById('items-per-category');
+    itemsPerCategory.textContent = `${data.length} items found for category ${categoryName} `;
+
 
 }
 
